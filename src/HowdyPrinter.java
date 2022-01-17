384244c8e732ed7cfd2b26d3f18900eb7b2fcec6
Dr. Michael J. Reale <realemj@sunypoly.edu>
2022-01-17 17:36:29 -0500

    Initial commit

---
+++ src/HowdyPrinter.java
@@ -0,0 +1,9 @@
+public class HowdyPrinter {
+    public static void main(String [] args) {
+        System.out.println("HENLO AND GREETS FRENS!");
+    }
+
+    public static String getPassword() {
+        return "1234";
+    }
+}
