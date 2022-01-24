3019e9067d86ba4c8fb2eabf570bc21464d3383c
Dr. Michael J. Reale <realemj@sunypoly.edu>
2022-01-24 14:00:12 -0500

    Another quick change to discuss imports

--- src/edu/realemj/Exercises_00/MyProgram.java
+++ src/edu/realemj/Exercises_00/MyProgram.java
@@ -1,5 +1,10 @@
 package edu.realemj.Exercises_00;
 
+import edu.realemj.Exercises_01.*;
+
 public class MyProgram {
+    public static void main(String [] args) {
+        Person p = new Person();
+    }
     
 }
