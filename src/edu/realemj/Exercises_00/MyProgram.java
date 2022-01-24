ad9c0322a36b7a14c108a4617da096c054d2123b
Dr. Michael J. Reale <realemj@sunypoly.edu>
2022-01-24 16:57:15 -0500

    Mild change to test something

--- src/edu/realemj/Exercises_00/MyProgram.java
+++ src/edu/realemj/Exercises_00/MyProgram.java
@@ -5,6 +5,7 @@ import edu.realemj.Exercises_01.*;
 public class MyProgram {
     public static void main(String [] args) {
         Person p = new Person();
+        Person q = new Person();
     }
     
 }
