4ef9af56f8e70fdac4a14e55f632dda3671ac4ed
CraigAmlin <93410838+CraigAmlin@users.noreply.github.com>
2022-01-26 12:47:26 -0500

    Completed Tididngs file for assign 1

---
+++ src/edu/amlinc/Assign01/Tidings.java
@@ -0,0 +1,10 @@
+package edu.amlinc.Assign01;
+
+public class Tidings {
+    public static void main(String[] args){
+        System.out.println("Reach for my hand, I'll soar away");
+        System.out.println("Into the dawn, oh , I wish I could stay");
+        System.out.println("Here in cherished halls, in peaceful days");
+        System.out.println("I fear the edge of dawn, knowing time betrays.");
+    }
+}
