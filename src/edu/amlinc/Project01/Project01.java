ec7a341c7df5125da2a72623edc499ffab1d50bd
CraigAmlin <93410838+CraigAmlin@users.noreply.github.com>
2022-02-21 13:49:42 -0500

    2-21-22 class adjustments

--- src/edu/amlinc/Project01/Project01.java
+++ src/edu/amlinc/Project01/Project01.java
@@ -2,11 +2,13 @@ package edu.amlinc.Project01;
 
 public class Project01 {
     public static void main(String [] args){
-        int [] arr;
-        int [] primes = {2,3,5,7,11,13,17,19}; 
-        arr = new int[5];
-        arr[0] = 7;
-        arr[1] = primes.length;
+        int [][] arr = new int[15][10];
+        int [] primes = {2,3,5,7,11,13,17,19};
+        for(int r = 0; r < arr.length; r++){
+            for(int c = 0; c < arr[r].length; c++){
+                arr[r][c] = 0;
+            }
+        }
         //for each loop (type instance: array)
         for(int p: primes){
             System.out.println(p);
